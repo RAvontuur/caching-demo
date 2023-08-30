5e87433d08cd7a00b1013809f251b5bbc9ca6bc8
ravontuur <rene.avontuur@outlook.com>
2023-08-30 19:12:37 +0200

    improved simulated database

--- src/main/java/com/github/ravontuur/cachingdemo/CachingDemoApplication.java
+++ src/main/java/com/github/ravontuur/cachingdemo/CachingDemoApplication.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @SpringBootApplication
@@ -45,17 +46,20 @@ class Content {
 @RestController
 @Slf4j
 class ContentController {
-
     @Autowired
     private ContentService contentService;
-
     private final AtomicInteger finishedCounter = new AtomicInteger();
-
     @GetMapping("/reset")
     public Boolean reset() {
         log.info("reset finished counter");
         finishedCounter.set(0);
         return true;
+    }
+
+    @GetMapping("/requests/count")
+    public Integer count() {
+        log.info("show counter");
+        return finishedCounter.get();
     }
 
     @GetMapping("/content/{id}")
@@ -75,21 +79,65 @@ class ContentController {
 @Slf4j
 class ContentService {
 
+    @Autowired
+    private Database database;
+
     @Cacheable(value = "content")
     public Content findContent(long id, int size, long duration) {
-        log.info("findContent({},{},{})",id, size, duration);
+        log.info("findContent({},{},{})", id, size, duration);
+        return database.executeQuery(id, size, duration);
+    }
+}
+
+@Service
+@Slf4j
+class Database {
+
+    public static final int N_DATABASE_THREADS = 20;
+    public static final int N_DATABASE_CPU = 1;
+    public static final int QUERY_TIMEOUT = 5000; // ms
+
+    ExecutorService executorService = Executors.newFixedThreadPool(N_DATABASE_THREADS);
+
+    private final AtomicInteger competingThreads = new AtomicInteger();
+
+    public Content executeQuery(long id, int size, long duration) {
+
+        competingThreads.incrementAndGet();
+        Future<Content> contentFuture = executorService.submit(() -> {
+            log.info("executeQuery({},{},{}, competing={})", id, size, duration, competingThreads.get());
+
+            long workToComplete = duration;
+            while (workToComplete > 200) {
+                workToComplete = workToComplete - workms(200);
+            }
+            workms(workToComplete);
+
+            return Content.builder()
+                    .id(id)
+                    .size(size)
+                    .duration(duration)
+                    .title(String.format("Title %d", id))
+                    .body("*".repeat(size))
+                    .build();
+        });
+
         try {
-            Thread.sleep(duration);
+            Content result = contentFuture.get(QUERY_TIMEOUT, TimeUnit.MILLISECONDS);
+            competingThreads.decrementAndGet();
+            return result;
+        } catch (InterruptedException | ExecutionException | TimeoutException e) {
+            int competing = competingThreads.getAndDecrement();
+            throw new RuntimeException("Failed id=" + id + ", competingThreads=" + competing, e);
+        }
+    }
+
+    private long workms(long duration) {
+        try {
+            Thread.sleep(duration * Long.max(1, (competingThreads.get() / N_DATABASE_CPU)));
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-
-        return Content.builder()
-                .id(id)
-                .size(size)
-                .duration(duration)
-                .title(String.format("Title %d", id))
-                .body("*".repeat(size))
-                .build();
+        return duration;
     }
 }
--- src/test/java/com/github/ravontuur/cachingdemo/CachingDemoApplicationTests.java
+++ src/test/java/com/github/ravontuur/cachingdemo/CachingDemoApplicationTests.java
@@ -1,10 +1,13 @@
 package com.github.ravontuur.cachingdemo;
-
+import io.netty.handler.timeout.ReadTimeoutException;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -17,9 +20,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 @Slf4j
 class CachingDemoApplicationTests {
 
-    private WebClient webClient = WebClient.builder().baseUrl("http://localhost:8080").build();
+    private WebClient webClient;
+    private WebClient webClientCounter;
 
+    private final AtomicLong totalNumberOfRequests = new AtomicLong(0L);
     private final AtomicLong totalDuration = new AtomicLong(0L);
+
+    private WebClient createWebClient(long responseTimeout, long port) {
+        return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(
+                        HttpClient.create()
+                                .responseTimeout(Duration.ofMillis(responseTimeout)))
+                )
+                .baseUrl("http://localhost:" + port).build();
+    }
 
     @AllArgsConstructor
     class TestCallable implements Callable<Content> {
@@ -33,8 +47,9 @@ class CachingDemoApplicationTests {
         public Content call() {
             long startTime = System.currentTimeMillis();
             Content result = webClient.get().uri(
-                    "/content/" + id + "?size=" + bodySize + "&duration=" + durationQuery)
+                            "/content/" + id + "?size=" + bodySize + "&duration=" + durationQuery)
                     .retrieve().bodyToMono(Content.class).block();
+            totalNumberOfRequests.incrementAndGet();
             totalDuration.addAndGet(System.currentTimeMillis() - startTime);
             log.info("received call {} with id {}", callId, id);
             return result;
@@ -42,13 +57,30 @@ class CachingDemoApplicationTests {
     }
 
     @Test
-    void testWithLoad() throws InterruptedException, ExecutionException {
+    void testWithLoad() throws InterruptedException {
 
-        int nCalls = 100;
-        int nThreads = 100;
-        long timeoutClient = 10000; // ms
-        int durationQuery = 2000; // ms
-        int bodySize = 5;
+        // load
+        int nCalls = 200;
+        int nThreads = 2;
+        int idOffset = 0; // start range of ids
+        int idMod = 1; // max number of ids (recycle)
+
+        // client side
+        long port = 8080;
+        long responseTimeoutClient = 10000; //ms
+
+        // server side
+        // see application.properties for server.tomcat.threads.max
+        int durationQuery = 1000; // ms
+        int bodySize = 5000; // bytes
+
+        // other
+        long timeoutLoadTest = 100000; // ms
+        long sleepBeforeCountStats = 1000; //ms
+        long responseTimeoutCountStats = 20000; //ms
+
+        webClient = createWebClient(responseTimeoutClient, port);
+        webClientCounter = createWebClient(responseTimeoutCountStats, port);
 
         log.info("started testWithLoad");
         assertThat(webClient.get().uri("/reset").retrieve().bodyToMono(Boolean.class).block()).isTrue();
@@ -58,30 +90,47 @@ class CachingDemoApplicationTests {
         Collection<TestCallable> callables = new ArrayList<>();
 
         for (int callId = 0; callId < nCalls; callId++) {
-            long id = callId;
+            long id = idOffset + callId % idMod;
             callables.add(new TestCallable(callId, id, durationQuery, bodySize));
         }
         log.info("Created {} callables", callables.size());
         // invoke all waits until all futures are completed or timeout expired
-        List<Future<Content>> futures = executorService.invokeAll(callables, timeoutClient, TimeUnit.MILLISECONDS);
+        List<Future<Content>> futures = executorService.invokeAll(callables, timeoutLoadTest, TimeUnit.MILLISECONDS);
         log.info("Invoked all {} callables", callables.size());
 
         log.info("Check successful completion");
         int nCancelled = 0;
+        int nExecutionExceptions = 0;
         for (int callId = 0; callId < nCalls; callId++) {
             Future<Content> future = futures.get(callId);
             if (future.isCancelled()) {
                 nCancelled++;
                 continue;
             }
-            Content result = future.get();
-
-            assertThat(result).isNotNull();
+            try {
+                Content result = future.get();
+                assertThat(result).isNotNull();
+            } catch (ExecutionException e) {
+                if (e.getCause().getCause() instanceof ReadTimeoutException) {
+                    log.info("Execution exception: ReadTimeoutException");
+                } else {
+                    log.info("Execution exception:", e.getCause());
+                }
+                nExecutionExceptions++;
+            }
         }
 
-        int nSuccessful = nCalls - nCancelled;
-        log.info("Successful calls: {}, Cancelled calls: {}", nSuccessful, nCancelled);
-        log.info("Query duration: {} ms", durationQuery);
-        log.info("Average duration successful calls: {} ms", totalDuration.get() / nSuccessful);
+        int nSuccessful = nCalls - nCancelled - nExecutionExceptions;
+        log.info("Successful calls: {}, Cancelled calls: {}, Execution exceptions: {}",
+                nSuccessful, nCancelled, nExecutionExceptions);
+        if (totalNumberOfRequests.get() == 0) {
+            log.info("Not any call with measured duration completed");
+        } else {
+            log.info("Average duration calls: {} ms", totalDuration.get() / totalNumberOfRequests.get());
+        }
+
+        Thread.sleep(sleepBeforeCountStats);
+        Long count = webClientCounter.get().uri("/requests/count").retrieve().bodyToMono(Long.class).block();
+        log.info("Number of requests handled by service: {}", count);
     }
 }
